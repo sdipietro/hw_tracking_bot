2a7fb6dd64c604ccd3cd40819a6cd0da959d3358
Stephen DiPietro <stevedipietro97@gmail.com>
2023-01-21 01:37:40 -0500

    Add script for running assessment bot. optimize grading script. Begin login for inputting scores.

--- assessment_bot.js
+++ assessment_bot.js
@@ -1,6 +1,7 @@
 const credentials = require("./credentials.js");
 const puppeteer = require('puppeteer');
 const { exec } = require("child_process");
+const { google } = require("googleapis");
 
 const progressTrackerScoresUrl = credentials.scoresUrl;
 
@@ -20,7 +21,21 @@ async function getScores(page) {
         let studentRows = Array.from(document.getElementsByTagName('tbody')[0].getElementsByTagName('tr'));
         studentRows.forEach((ele) => {
             let name = ele.getElementsByTagName('td')[1].getElementsByTagName('a')[0].innerText;
-            let submissionLink = ele.getElementsByTagName('td')[3].getElementsByTagName('a')[0];
+            let submissionsArr = ele.getElementsByTagName('td')[3].getElementsByTagName('a');
+            let submissionLink = '';
+            if (submissionsArr && submissionsArr.length > 1) {
+                let longestTime = 0;
+                
+                Array.from(submissionsArr).forEach(a => {
+                    let time = parseInt(a.innerText.split('(')[1].split(' ')[0]);
+                    if (time >= longestTime) {
+                        longestTime = time;
+                        submissionLink = a;
+                    }
+                });
+            } else {
+                submissionLink = ele.getElementsByTagName('td')[3].getElementsByTagName('a')[0];
+            }
             
             if (submissionLink) {
                 scoresObj[name] = submissionLink.href;
@@ -33,7 +48,7 @@ async function getScores(page) {
     return studentList;
 }
 
-function gradeAssessments(assessmentLinksObj) {
+async function gradeAssessments(assessmentLinksObj) {
     let scores = {};
     let links = Object.values(assessmentLinksObj);
 
@@ -42,22 +57,92 @@ function gradeAssessments(assessmentLinksObj) {
         link.pop();
         let newLink = link.join('');
         let newName = name.split(' ').join('');
-        let command = `/Users/steve/Desktop/hw_tracking_bot/downloadAssessmentScript.sh "${newLink}" "${newName}"`
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
+        let command = `/Users/steve/Desktop/hw_tracking_bot/downloadAssessmentScript.sh "${newLink}" "${newName}"`;
+        const result = await new Promise((resolve, reject) => {
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                }
+                // if (stderr) {
+                //     console.log(`stderr: ${stderr}`);
+                // }
+                resolve(stdout);
+            });
         });
+
+        let specs = result.split(' : ')[1];
+        let examples;
+        let failures;
+        let grade;
+        if (specs.includes('failures')) {
+            examples = parseInt(specs.split(' examples,')[0]);
+            failures = parseInt(specs.split(', ')[1].split(' ')[0]);
+            grade = examples - failures;
+            scores[name] = grade;
+        } else {
+            scores[name] = 0;
+        }
+        console.log(`${result}`);
+    }
+    return scores;
+}
+
+async function inputScoresGoogle(scoresData) {
+    console.log('Inputting scores in google sheets...')
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "/Users/steve/Desktop/hw_tracking_bot/google_creds.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const spreadsheetId = "1sKwWeKPq8LP8qYiU6-T1dMyHUkI-YovyLz40SV-c6eU";
+    let assessName = 'Fo1';
+    let startingCol;
+
+    switch (true) {
+        case assessName == 'Fo1':
+            startingCol = 'F';
+            break;
+        case assessName == 'Fo2':
+            startingCol = 'G';
+            break;
+        default:
+            console.log('Error: Wrong Assessment Name.');
     }
 
-    links.forEach(link => {
-        
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: 'Sheet 1!A2:A41',
+    });
+    const studentArr = getRows.data.values;
+    const gooogleSheetStudentsIndexes = {};
+  
+    for (let i = 0; i < studentArr.length; i++) {
+        gooogleSheetStudentsIndexes[studentArr[i][0]] = i;
+    }
+
+    let formattedScoreData = [];
+    for (const student in gooogleSheetStudentsIndexes) {
+        let name = student;
+        if (student.includes(' (')) {
+            let first = student.split('(')[1].split(')')[0];
+            let last = student.split(') ')[1];
+            name = first + ' ' + last;
+        }
+        formattedScoreData.push([(scoresData[name] || '')]);
+    }
+    
+
+    await googleSheets.spreadsheets.values.update({
+        auth,
+        spreadsheetId,
+        range: `Sheet 1!${startingCol}2:${startingCol}50`,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: formattedScoreData
+        },
     });
 }
 
@@ -68,12 +153,13 @@ async function updateScores(){
     const loggedInPT = await loginPT(page);
     console.log('Getting Scores...');
     const scoresData = await getScores(loggedInPT);
-    gradeAssessments(scoresData);
-    // console.log(scoresData);
-    // console.log('Closing Virtual Browser...');
-    // await page.close();
-    // await browser.close();
-    // console.log('Done!');
+    await page.close();
+    await browser.close();
+    console.log('Grading Assessments...');
+    console.log('');
+    const scores = await gradeAssessments(scoresData);
+    // console.log(scores);
+    // await inputScoresGoogle(scores);
 }
 
 updateScores();
